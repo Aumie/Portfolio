19a8eb9d449822d56ed90b2a16f91402196d7a22
Aumie <aum5376@gmail.com>
2023-11-09 00:08:04 +0700

    update info

--- frontend/src/pages/About/data.js
+++ frontend/src/pages/About/data.js
@@ -1,27 +1,39 @@
 export const job = [
-  {
-    id: 'recIL6mJNfWObonls',
-    order: 2,
-    title: 'Jr. Solution Architect',
-    dates: 'July 2021 - Mar 2022',
-    duties: [
-      'Provided quotations and service mapping architecture diagram to the customers after discussing with Sr. Solution Architects and Business Developments.',
-      'Supported the customers by providing documentation or instructions through online meeting, when there are technical issues on products.',
-      'Delivered solutions to the customer with Cloud Engineers.',
-      'Worked off-site or online with a Sr. Solution Architect on requested Hands-On Labs from the customers.',
-    ],
-    company: 'Huawei',
-  },
-  {
-    id: 'rec61x18GVY99hQq5',
-    order: 1,
-    title: 'Software Engineer (Internship)',
-    dates: 'Jun 2020 - Sep 2020',
-    duties: [
-      'Successfully developed a security system which IP cameras are used to detect and calculate visitors, entering or leaving a building, by using deep learning.',
-      'Independently experimented with test frameworks i.e. MStest, Nunit, unittest, googletest.',
-      'Experienced in creating CI/CD pipeline with Jenkins and Docker.',
-    ],
-    company: 'NDR Solution',
-  },
+	{
+		id: 'recIL6mJNfWObon69',
+		order: 3,
+		title: 'Unreal Engine Developer',
+		dates: 'Sep 2023 - Oct 2023',
+		duties: [
+			'Implemented game mechanics',
+			'Implemented a simple GOAP AI Framework to use in an Unreal Engine project',
+			"Implemented a VR plugin to be used in V-mall project. Features: Changing product's color automaticcally while picking from a carousel with simple replaceable UI",
+		],
+		company: 'AloneClone',
+	},
+	{
+		id: 'recIL6mJNfWObonls',
+		order: 2,
+		title: 'Jr. Solution Architect',
+		dates: 'July 2021 - Mar 2022',
+		duties: [
+			'Provided quotations and service mapping architecture diagram to the customers after discussing with Sr. Solution Architects and Business Developments.',
+			'Supported the customers by providing documentation or instructions through online meeting, when there are technical issues on products.',
+			'Delivered solutions to the customer with Cloud Engineers.',
+			'Worked off-site or online with a Sr. Solution Architect on requested Hands-On Labs from the customers.',
+		],
+		company: 'Huawei',
+	},
+	{
+		id: 'rec61x18GVY99hQq5',
+		order: 1,
+		title: 'Software Engineer (Internship)',
+		dates: 'Jun 2020 - Sep 2020',
+		duties: [
+			'Successfully developed a security system which IP cameras are used to detect and calculate visitors, entering or leaving a building, by using deep learning.',
+			'Independently experimented with test frameworks i.e. MStest, Nunit, unittest, googletest.',
+			'Experienced in creating CI/CD pipeline with Jenkins and Docker.',
+		],
+		company: 'NDR Solution',
+	},
 ]
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -9,90 +9,91 @@ import { axiosInstance } from '../../utils'
 const projectUrl = '/blogs/post/?page_size=3&tags=project'
 const postUrl = '/blogs/post/?page_size=3'
 export const Home = () => {
-  const [loading, setLoading] = useState(true)
-  const [proj, setProj] = useState([])
-  const [blogs, setBlogs] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [proj, setProj] = useState([])
+	const [blogs, setBlogs] = useState([])
 
-  const fetchBlogs = async () => {
-    const resP = await axiosInstance.get(projectUrl)
-    const resB = await axiosInstance.get(postUrl)
-    setProj(resP.data.results)
-    setBlogs(resB.data.results)
-    setLoading(false)
-  }
-  useEffect(() => {
-    fetchBlogs()
-    document.title = "Aum Peerapat"
-  }, [])
+	const fetchBlogs = async () => {
+		const resP = await axiosInstance.get(projectUrl)
+		const resB = await axiosInstance.get(postUrl)
+		setProj(resP.data.results)
+		setBlogs(resB.data.results)
+		setLoading(false)
+	}
+	useEffect(() => {
+		fetchBlogs()
+		document.title = 'Aum Peerapat'
+	}, [])
 
-  return (
-    <React.Fragment>
-      <div className='jumbotron'>
-        <div className='container text-center'>
-          <div className='p-5 mb-4 rounded-3'>
-            <div className='py-5'>
-              <h1 className='display-5 fw-bold'>Hi, I'm Peerapat (Aum)</h1>
-              <p className='fs-4'>
-                <span className='tab'>
-                  After some reflection for a while, it has come to a conclusion
-                  that what I have been looking for is to work in the game
-                  industry.<br/>
-                  My first step upon this journey is in your hand.
-                </span>
-              </p>
-              <button className='btn btn-warning rgb' type='button'>
-                <a
-                  href='https://aumidev.me/static/media/uploads/AumProfile/cv_Peerapat_Channak.pdf'
-                  target='_blank'
-                  style={{ color: 'white' }}
-                >
-                  CV Download
-                </a>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      {loading ? (
-        <Loading loading={loading} />
-      ) : (
-        <ThreeColumns>
-          <section className='card-section'>
-            <p>recent projects</p>
-            {proj.map((obj, index) => {
-              return <Card parent='home' obj={obj} key={index} />
-            })}
+	return (
+		<React.Fragment>
+			<div className='jumbotron'>
+				<div className='container text-center'>
+					<div className='p-5 mb-4 rounded-3'>
+						<div className='py-5'>
+							<h1 className='display-5 fw-bold'>Hi, I'm Peerapat (Aum)</h1>
+							<p className='fs-4'>
+								<span
+								// className='tab'
+								>
+									I love everything about 3D.
+									<br />
+									3D is my lifelong journy.
+								</span>
+							</p>
+							<button className='btn btn-warning rgb' type='button'>
+								<a
+									href='https://aumidev.me/static/media/uploads/AumProfile/cv_Peerapat_Channak.pdf'
+									target='_blank'
+									style={{ color: 'white' }}
+								>
+									CV Download
+								</a>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			{loading ? (
+				<Loading loading={loading} />
+			) : (
+				<ThreeColumns>
+					<section className='card-section'>
+						<p>recent projects</p>
+						{proj.map((obj, index) => {
+							return <Card parent='home' obj={obj} key={index} />
+						})}
 
-            <div className='d-flex justify-content-end'>
-              <p>
-                <Link
-                  to={'/blog/?tags=project'}
-                  className='rgb'
-                  style={{ color: 'var(--clr-font)' }}
-                >
-                  view more
-                </Link>
-              </p>
-            </div>
-          </section>
-          <section className='blog-section'>
-            <article className='full-column'>
-              <div className='article-wrapper'>
-                <div className='blog-label'>
-                  <div className='home-label rgb'>
-                    <Link to={'/blog'}>
-                      <h4 className='home-label-title'>BLOG</h4>
-                    </Link>
-                  </div>
-                </div>
-                {blogs.map((obj, index) => {
-                  return <BlogsContent obj={obj} key={index} />
-                })}
-              </div>
-            </article>
-          </section>
-        </ThreeColumns>
-      )}
-    </React.Fragment>
-  )
+						<div className='d-flex justify-content-end'>
+							<p>
+								<Link
+									to={'/blog/?tags=project'}
+									className='rgb'
+									style={{ color: 'var(--clr-font)' }}
+								>
+									view more
+								</Link>
+							</p>
+						</div>
+					</section>
+					<section className='blog-section'>
+						<article className='full-column'>
+							<div className='article-wrapper'>
+								<div className='blog-label'>
+									<div className='home-label rgb'>
+										<Link to={'/blog'}>
+											<h4 className='home-label-title'>BLOG</h4>
+										</Link>
+									</div>
+								</div>
+								{blogs.map((obj, index) => {
+									return <BlogsContent obj={obj} key={index} />
+								})}
+							</div>
+						</article>
+					</section>
+				</ThreeColumns>
+			)}
+		</React.Fragment>
+	)
 }
